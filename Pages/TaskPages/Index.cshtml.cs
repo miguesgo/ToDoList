573244dbe7f453fe6a053a949cd0d82c48bd82fb
miguesgo <miguesturtle@outlook.com>
2025-03-14 19:14:06 -0600

    The txt file download already works well

--- Pages/TaskPages/Index.cshtml.cs
+++ Pages/TaskPages/Index.cshtml.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 using ToDoList.Data;
 using ToDoList.Model;
 
@@ -19,19 +20,12 @@ namespace ToDoList.Pages.TaskPages
         [BindProperty(SupportsGet = true)]
         public string SortOrder { get; set; } = "date"; // Default sorting by Name
 
+        [BindProperty(SupportsGet = true)]
+        public string DownloadType { get; set; } = "txt"; //Default downloading by txt file
+
         public async Task<IActionResult> OnGetAsync()
         {
-            var query = _db.ToDoTask.AsQueryable();
-
-            query = SortOrder switch
-            {
-                "name" => query.OrderBy(t => t.Name),
-                "date" => query.OrderBy(t => t.DueDate),
-                "status" => query.OrderBy(t => t.IsCompleted),
-                _ => query.OrderBy(t => t.Name)
-            };
-
-            ToDoTasks = await query.ToListAsync();
+            ToDoTasks = await SortedList();
 
             // If it's an AJAX request, return only the sorted table
             if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
@@ -40,6 +34,34 @@ namespace ToDoList.Pages.TaskPages
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetDownloadTasksAsync() //Save in file
+        {
+            ToDoTasks = await SortedList();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("List of tasks");
+            stringBuilder.AppendLine("=======================");
+            foreach (var t in ToDoTasks)
+            {
+                stringBuilder.AppendLine($"{t.Id}. [{(t.IsCompleted ? "✔" : " ")}] {t.Name}: {t.Description} {t.DueDate}");
+            }
+            var bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+            var fileName = "To-Do-List.txt";
+            return File(bytes, "text/plain", fileName);
+        }
+
+        public async Task<List<ToDoTask>> SortedList()
+        {
+            var query = _db.ToDoTask.AsQueryable();
+            query = SortOrder switch
+            {
+                "name" => query.OrderBy(t => t.Name),
+                "date" => query.OrderBy(t => t.DueDate),
+                "status" => query.OrderBy(t => t.IsCompleted),
+                _ => query.OrderBy(t => t.Name)
+            };
+            return await query.ToListAsync();
         }
     }
 }
